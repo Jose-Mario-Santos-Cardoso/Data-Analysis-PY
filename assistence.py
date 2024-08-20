be7c144f3a175a1369f30f7d822956e85eb5a1cf
Jose-Mario-Santos-Cardoso <jose.cardoso@uscsonline.com.br>
2024-08-20 10:19:24 -0300

    Add files via upload

---
+++ assistence.py
@@ -0,0 +1,7 @@
+#pip install pandas
+#pip install openpyxl
+#import a db
+#pip install pandas openpyxl numpy ipykernel nbformat plotly
+# !pip install - {[(instead of %pip install)]} - could install packages into the wrong environment
+#!pip install pandas openpyxl numpy ipykernel nbformat plotly
+#%pip install pandas openpyxl numpy ipykernel nbformat plotly 
